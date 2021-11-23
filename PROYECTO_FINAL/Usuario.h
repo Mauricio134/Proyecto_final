bbc6a6bd02ed03ba96dcb727a4b586a908a7080e
maumallma1 <85748915+maumallma1@users.noreply.github.com>
2021-11-23 10:39:47 -0500

    Update Usuario.h

--- PROYECTO_FINAL/Usuario.h
+++ PROYECTO_FINAL/Usuario.h
@@ -6,10 +6,24 @@ using namespace std;
 class Usuario{
     public:
         //Variables
-        Administrador admin;
-        Cliente cli;
-        //Constructor
-        Usuario();
+		string usuario; //cliente y admin
+        string contrasena; //cliente y admin
+        string dni; //cliente
+        string id; //admin
+        string telefono; //cliente
+        string direccion; //cliente
+		string usuaux;
+        string contraux;
+        string dniaux;
+        bool encontrado;
+		Pedidos pedido;
         //Metodos
         int menu_usu();
+		void registrarse_cli();
+        void registrarse_admin();
+        void mostrar_registro();
+		void loguearse_cli();
+		void loguearse_admin();
+		void eliminar();
+		void mostrar();
 };
